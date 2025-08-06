cf3f27e3f93a32f1dd936f8da2691209d5b0a320
ali <aliabbasi.95@yahoo.com>
2025-08-06 15:45:30 +0330

    resolve bug

--- wallets/utils/consts.py
+++ wallets/utils/consts.py
@@ -18,4 +18,4 @@ DEFAULT_WALLETS = {
 }
 
 FRONTEND_PAYMENT_DETAIL_URL = "dashboard/payment/"
-FRONTEND_INSTALLMENT_REQUEST_DETAIL_URL = "dashboard/payment/"
+FRONTEND_INSTALLMENT_REQUEST_DETAIL_URL = "dashboard/installments/"
