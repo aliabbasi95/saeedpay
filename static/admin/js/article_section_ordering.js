5bd38d6acdc34504e27b551798a3bb7e253c6a2b
Reyhaneh <reyhoondavaee@gmail.com>
2026-04-21 15:04:44 +0330

    static pre-commit

--- static/admin/js/article_section_ordering.js
+++ static/admin/js/article_section_ordering.js
@@ -1,11 +1,11 @@
 (function($) {
     'use strict';
-    
+
     function updateSectionOrdering() {
         // Find all order fields in the article section inline
         var orderFields = $('.field-order input[type="number"]');
         var maxOrder = 0;
-        
+
         // Find the highest existing order value
         orderFields.each(function() {
             var value = parseInt($(this).val()) || 0;
@@ -13,12 +13,12 @@
                 maxOrder = value;
             }
         });
-        
+
         // Set order for empty fields
         orderFields.each(function() {
             var $field = $(this);
             var currentValue = parseInt($field.val()) || 0;
-            
+
             // If field is empty or 0, assign next order
             if (currentValue === 0) {
                 maxOrder++;
@@ -26,11 +26,11 @@
             }
         });
     }
-    
+
     function initializeSectionOrdering() {
         // Update ordering when page loads
         updateSectionOrdering();
-        
+
         // Watch for new inline forms being added
         $(document).on('formset:added', function(event, $row) {
             // Check if this is an article section inline
@@ -40,7 +40,7 @@
                 }, 100);
             }
         });
-        
+
         // Also handle the "Add another" button click
         $(document).on('click', '.add-row a', function() {
             var $inline = $(this).closest('.inline-group');
@@ -51,10 +51,10 @@
             }
         });
     }
-    
+
     // Initialize when DOM is ready
     $(document).ready(function() {
         initializeSectionOrdering();
     });
-    
+
 })(django.jQuery);
